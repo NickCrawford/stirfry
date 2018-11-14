8c74d8020a092b1dee40b5a61ecc5282f108cef9
Ben Holland <bhollan5@kent.edu>
2018-11-14 14:59:14 -0500

    Portfolio top mobile implemented 📱

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import Portfolio from "./views/Portfolio.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
       path: "/portfolio",
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,15 +1,62 @@
 <template>
   <div id="portfolio">
-    <h1>This is a portfolio page</h1>
+    <div id="portfolio-top">
+      <h1>
+        Our Dishes <br>
+        <b>So Far...</b>
+      </h1>
+      <p>These are some of our past projects. </p>
+      <p>Scroll down or click on a project to see more. </p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-@import '@/GlobalVars.scss';
+@import "@/GlobalVars.scss";
 
 #portfolio {
-    background: $background;
-    height: 100vh;
-    width: 100%;
+  background: $white;
+  height: 100vh;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 40% 60%;
+  overflow: hidden;
+}
+
+h1 {
+  font-weight: normal;
+  font-size: 3em;
+  margin: 30px;
+  position: relative;
+  z-index: 10;
+}
+p {
+  opacity: 0.5;
+  z-index: 10;
+  position: relative;
+}
+
+#portfolio-top {
+  background: $green;
+  color: white;
+  grid-column: 1/3;
+  position: relative;
+  z-index: 1;
+}
+#portfolio-top::after {
+  content: "";
+  position: absolute;
+  left: -10%;
+  bottom: -15%;
+  width: 120%;
+  height: 200px;
+  background-color: $green;
+  transform: rotate(12deg);
+  z-index: 0;
+}
+
+@media only screen and (min-width: 600px) {
+
 }
 </style>
