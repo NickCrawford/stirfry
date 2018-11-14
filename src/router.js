a565deebd02444f4d2c6a98f2de84b291c372beb
Ben Holland <bhollan5@kent.edu>
2018-11-14 14:12:58 -0500

    Added portfolio page, color variables 🎨

--- src/router.js
+++ src/router.js
@@ -2,11 +2,18 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import Portfolio from "./views/Portfolio.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
+    {
+      path: "/portfolio",
+      name: "portfolio",
+      component: Portfolio
+    },
     {
       path: "/",
       name: "home",
---
+++ src/views/Portfolio.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="portfolio">
+    <h1>This is a portfolio page</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/GlobalVars.scss';
+
+#portfolio {
+    background: $background;
+    height: 100vh;
+    width: 100%;
+}
+</style>
