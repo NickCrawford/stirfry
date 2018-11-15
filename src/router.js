31a77f735a4a8bd1437360820d86a5e07371a2a0
NickCrawford <hi@nickcrawford.design>
2018-11-14 19:25:21 -0500

    Adds 404 route

--- src/router.js
+++ src/router.js
@@ -2,13 +2,14 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import FourOhFour from "./views/FourOhFour.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/",
+      path: "/home",
       name: "home",
       component: Home
     },
@@ -16,6 +17,11 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "/*",
+      name: "404",
+      component: FourOhFour
     }
   ]
 });
---
+++ src/views/FourOhFour.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="four-oh-four">
+    <h1>Oh no!</h1>
+    <h3>This page doesn't exist.</h3>
+  </div>
+</template>
+
+<style lang="css">
+.four-oh-four {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  color: white;
+}
+</style>
+
