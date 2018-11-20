4f78fa2f8e97a4f2cd0214e096967a1609c9d07c
NickCrawford <hi@nickcrawford.design>
2018-11-19 23:31:19 -0500

    Fix vue config for prerender

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,21 @@
-var path = require("path");
-
+const path = require("path");
 const PrerenderSPAPlugin = require("prerender-spa-plugin");
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
-// // vue.config.js
 module.exports = {
-  configureWebpack: {
-    // externals: {
-    //   oimo: false,
-    //   cannon: true,
-    //   earcut: false
-    // },
-    plugins: [
-      new PrerenderSPAPlugin({
-        staticDir: path.join(__dirname, "dist"),
-        routes: ["/", "/about", "/contact"],
-
-        renderer: new Renderer({
-          inject: {
-            foo: "bar"
-          },
-          headless: false,
-          renderAfterDocumentEvent: "render-event"
-        })
-      })
-    ]
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== "production") return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, "dist"),
+          // List of routes to prerender
+          ["/", "/contact", "/about"],
+          {
+            // options
+          }
+        )
+      ]
+    };
   }
 };
