00f22e63bbf0a856366332505edf4c1ad262ee53
NickCrawford <hi@nickcrawford.design>
2018-11-19 21:42:49 -0500

    Adds about page copy

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section>
+      <h1>About us</h1>
+      <h2>2018</h2>
+      <h3>Spring - Officially open for business</h3>
+      <p>Startup Stirfry LLC was officially founded by Nick Crawford and Ben Holland. Their first task: establish themselves as professional web designers. Together they started on their very first agency project, <a href="http://www.sellout.at">Sellout</a></p>
+    </section>
+    <section>
+      <h3>Summer - Heating up...</h3>
+      <p>During the summer, Nick and Ben found themselves personally taking on various freelance projects. As these projects began to stack up, both their depth and breadth of work expanded. They learned new skills, technologies, and best-practices. It was about time to put these new skills to the test. It was time to launch an official agency.</p>
+    </section>
+    <section>
+      <h3>Winter - Gettin' creative</h3>
+      <p>With several major projects under their belts, Startup Stirfry is now an established creative agency dedicated to helping businesses find their visual identity. We even launched a new portfolio website to celebrate!</p>
+    </section>
   </div>
 </template>
+
+<style scoped>
+.about {
+  padding: 2rem 5vw;
+}
+
+section {
+  width: 100%;
+  max-width: 1040px;
+  margin: 0 auto;
+  text-align: left;
+}
+</style>
+
