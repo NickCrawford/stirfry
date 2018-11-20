0c391ea372f25875ddf3cdc0a9a047139319f341
NickCrawford <hi@nickcrawford.design>
2018-11-20 00:36:51 -0500

    Adds encode method for form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -74,6 +74,14 @@ export default {
   },
 
   methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join("&");
+    },
+
     handleSubmit() {
       fetch("/", {
         method: "POST",
