29baab3079bb4181ea4b9d9c2d49b91c0ce11793
Ben Holland <bhollan5@kent.edu>
2019-03-30 15:20:59 -0400

    Installing Babel

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,21 @@ module.exports = {
             // options
           }
         )
-      ]
+      ],
+      module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
     };
   }
 };
